9065e340c4ed14a6f86e789ec86eb1e61166ae36
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-03-03 03:46:06 +0100

    Add views

---
+++ organizer/forms.py
@@ -0,0 +1,73 @@
+from typing import Optional
+
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from organizer.models import Info, Doer, Task
+
+
+class TaskForm(forms.ModelForm):
+    doers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+
+class DoerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Doer
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+        )
+
+
+class DoerSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"})
+    )
+
+
+class DoerUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Doer
+        fields = ["first_name", "last_name"]
+
+    def clean_license_number(self) -> Optional[str]:
+        return self.cleaned_data["first_name", "last_name"]
+
+
+class TaskSearchForm(forms.Form):
+    description = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search"})
+    )
+
+
+class InfoForm(forms.ModelForm):
+    doers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Info
+        fields = "__all__"
+
+
+class InfoSearchForm(forms.Form):
+    task_category = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by task category"})
+    )
