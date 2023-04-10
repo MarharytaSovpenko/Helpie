48a8090850bbae7e661c499985d8ef2f89537c20
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-04-10 23:30:07 +0200

    Update settings, .env.sample and requirements.txt

--- helpie/settings.py
+++ helpie/settings.py
@@ -23,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-saqk7*x5oj0dj%v6kwjji+^_+04=2oq==in5ypsg8vtox=!s_8"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
