52562d055220fc05fd63efcbfc1b01bb3b50b8eb
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-04-11 15:24:59 +0200

    Remove https from the render host

--- helpie/settings.py
+++ helpie/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://helpie.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "helpie.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
